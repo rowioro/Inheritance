f91e4d8bd73364be1a814c5bd14439acdbd847a9
Robert Stefaniak <01138659@pw.edu.pl>
2020-06-01 18:51:39 +0200

    Add project files.

---
+++ ConsoleApp15_Dziedziczenie/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15_Dziedziczenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ziemia ziemia = new Ziemia();
+            ziemia.Atmosfera();
+            ziemia.PoraDniaNocy();
+            ziemia.Ruch();
+            Console.ReadKey();
+        }
+    }
+
+    abstract class CialoNiebieskie
+    {
+        public string Nazwa { get; set; }
+        public void Ruch()
+        {
+            Console.WriteLine("Kazdemu cialu niebieskiemu towarzyszy ruch.");
+        }
+    }
+    abstract class Planeta: CialoNiebieskie
+    {
+        public void PoraDniaNocy()
+        {
+            Console.WriteLine("Pora dnia i nocy.");
+        }
+    }
+    class Ziemia: Planeta
+    {
+        public void Atmosfera()
+        {
+            Console.WriteLine("Atmosfera ziemska.");
+        }
+    }
+}
